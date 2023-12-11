0ebe7a69f1ae9cc1d7db2fab604bba05cbb3e90d
Adeshina Qudus <qudusa55@gmail.com>
2023-12-11 12:07:57 +0100

    commit

---
+++ src/w3resource/Hello.java
@@ -0,0 +1,8 @@
+package w3resource;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("Alexandra Abramov");
+    }
+}
