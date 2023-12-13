97ff53b3cb028c501ab1824c9815e70d742df8ed
Adeshina Qudus <qudusa55@gmail.com>
2023-12-13 12:09:58 +0100

    commit

---
+++ src/w3resource/SumOfTwo.java
@@ -0,0 +1,10 @@
+package w3resource;
+
+public class SumOfTwo {
+    public static void main(String[] args) {
+
+        int firstNumber = 74;
+        int secondNumber = 36;
+        System.out.println(firstNumber+secondNumber);
+    }
+}
