360594001f1587c7dbd1a603566e6932c1a3fe94
Adeshina Qudus <qudusa55@gmail.com>
2023-12-14 12:13:15 +0100

    commit

---
+++ src/w3resource/DivideTwoNumbers.java
@@ -0,0 +1,11 @@
+package w3resource;
+
+public class DivideTwoNumbers {
+
+    public static void main(String[] args) {
+
+        int number = 50;
+        int divisor = 3;
+        System.out.println(number/divisor);
+    }
+}
