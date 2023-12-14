2c03762faee06b0c5c167e12295e31e91f5726eb
Adeshina Qudus <qudusa55@gmail.com>
2023-12-14 12:16:43 +0100

    commit

--- src/w3resource/DivideTwoNumbers.java
+++ src/w3resource/DivideTwoNumbers.java
@@ -3,7 +3,6 @@ package w3resource;
 public class DivideTwoNumbers {
 
     public static void main(String[] args) {
-
         int number = 50;
         int divisor = 3;
         System.out.println(number/divisor);
---
+++ src/w3resource/ProductOfTwoNumbers.java
@@ -0,0 +1,10 @@
+package w3resource;
+
+public class ProductOfTwoNumbers {
+    public static void main(String[] args) {
+
+        int firstNumber = 25;
+        int secondNumber = 5;
+        System.out.println(firstNumber*secondNumber);
+    }
+}
