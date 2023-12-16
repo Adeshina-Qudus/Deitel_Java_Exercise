fd3f538a08ee7fc51f363cd73a60055a51d99715
Adeshina Qudus <qudusa55@gmail.com>
2023-12-16 12:20:13 +0100

    commit

---
+++ src/w3resource/Mod.java
@@ -0,0 +1,10 @@
+package w3resource;
+
+public class Mod {
+    public static void main(String[] args) {
+
+        int firstNumber = 125;
+        int secondNumber = 24;
+        System.out.println(firstNumber % secondNumber);
+    }
+}
