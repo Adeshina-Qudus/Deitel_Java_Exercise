5fcc7e13b1b94825b9bccd121e9d3ca8235a7438
Adeshina Qudus <qudusa55@gmail.com>
2023-12-17 12:24:09 +0100

    commit

---
+++ src/w3resource/Multiplication.java
@@ -0,0 +1,22 @@
+package w3resource;
+
+import java.util.Scanner;
+
+public class Multiplication {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int inputNumber  = input.nextInt();
+        System.out.println(inputNumber);
+        System.out.println(inputNumber * 2);
+        System.out.println(inputNumber * 3);
+        System.out.println(inputNumber * 4);
+        System.out.println(inputNumber * 5);
+        System.out.println(inputNumber * 6);
+        System.out.println(inputNumber * 7);
+        System.out.println(inputNumber * 8);
+        System.out.println(inputNumber * 9);
+        System.out.println(inputNumber * 10d);
+
+    }
+
+}
