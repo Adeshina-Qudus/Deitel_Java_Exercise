792aa6281c9f3428214d9516311bdd8a1e2a94d9
Adeshina Qudus <qudusa55@gmail.com>
2023-12-18 19:02:29 +0100

    commit

---
+++ src/w3resource/SpecifiedExpression.java
@@ -0,0 +1,13 @@
+package w3resource;
+
+public class SpecifiedExpression {
+    public static void main(String[] args) {
+
+        int number = 100;
+        int secondNumber = 0;
+        int divisor = 3;
+        int result = (number + secondNumber ) /divisor;
+        System.out.println(result);
+
+    }
+}
