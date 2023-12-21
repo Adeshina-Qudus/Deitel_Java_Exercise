d2259d6764d6ffe826c00d7518ddd37cdcae9c16
Adeshina Qudus <qudusa55@gmail.com>
2023-12-21 19:30:19 +0100

    commit

---
+++ src/w3resource/BooleanArray.java
@@ -0,0 +1,11 @@
+package w3resource;
+
+import java.util.Arrays;
+
+public class BooleanArray {
+    public static void main(String[] args) {
+      boolean [][] array = {{true, false, true},
+                {false, true, false}};
+        System.out.println(Arrays.deepToString(array));
+    }
+}
