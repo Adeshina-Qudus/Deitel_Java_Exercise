ff0faa9749b5c128bb4712b7f678569b0fe16bcf
Adeshina Qudus <qudusa55@gmail.com>
2024-02-05 10:17:06 +0100

    commit

--- src/RegexExample.java
+++ src/RegexExample.java
@@ -3,15 +3,21 @@ import java.util.ArrayList;
 public class RegexExample {
 
 
-//    public static String[] regex(String[] arrayInput) {
-//        ArrayList<String> arrayList = new ArrayList<>();
-//        for (String element : arrayInput){
-//            if (element.toUpperCase().matches("^C")){
-//                arrayList.add(element);
-//            }
-//        }
-//        return toArray(arrayList);
-//    }
-//
-//// git
+    public static String[] regex(String[] arrayInput) {
+        ArrayList<String> arrayList = new ArrayList<>();
+        for (String element : arrayInput){
+            if (element.toUpperCase().matches("^C")){
+                arrayList.add(element);
+            }
+        }
+        return toArray(arrayList);
+    }
+
+    private static String[] toArray(ArrayList<String> arrayList) {
+        String [] result = new String[arrayList.size()];
+        for (int element = 0; element < arrayList.size(); element++){
+            result[element] = arrayList.get(element);
+        }
+        return result;
+    }
 }
