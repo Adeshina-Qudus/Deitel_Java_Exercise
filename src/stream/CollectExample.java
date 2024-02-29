7f981e5dca43dc6365168d0288b64f1c29acefd9
Adeshina Qudus <qudusa55@gmail.com>
2024-02-29 17:04:05 +0100

    commit

---
+++ src/stream/CollectExample.java
@@ -0,0 +1,19 @@
+package stream;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class CollectExample {
+    public static void main(String[] args) {
+
+        Set<Integer> integers = Set.of(100,2,30,4,5,1);
+
+
+        List<Integer> integers1 = integers.stream()
+                .distinct()
+                .sorted()
+                .collect(Collectors.toList());
+        System.out.println(integers1);
+    }
+}
