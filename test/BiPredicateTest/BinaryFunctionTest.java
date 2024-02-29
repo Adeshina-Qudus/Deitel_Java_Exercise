da5b3449d7427582dbb5e3d4c3cefed320c57f3b
Adeshina Qudus <qudusa55@gmail.com>
2024-02-29 16:19:39 +0100

    commit

--- test/BiPredicateTest/BinaryFunctionTest.java
+++ test/BiPredicateTest/BinaryFunctionTest.java
@@ -1,12 +1,6 @@
 package BiPredicateTest;
-
 import org.junit.jupiter.api.Test;
-
-import java.util.function.LongToDoubleFunction;
-
 public class BinaryFunctionTest {
-
-
     @Test
     public void binaryFunctionTest(){
     }
