ae6581318443d70f2356ea4abbcf86158f75d195
Adeshina Qudus <qudusa55@gmail.com>
2024-02-29 16:05:56 +0100

    commit

---
+++ src/stream/FlatMapExample.java
@@ -0,0 +1,19 @@
+package stream;
+
+import java.util.List;
+
+public class FlatMapExample {
+
+
+    public static void main(String[] args) {
+
+        List<List<Integer>> lists = List.of(
+                List.of(10,20,30),
+                List.of(100,200,300),
+                List.of(1000,2000,3000)
+        );
+        lists.stream()
+                .flatMap((list) -> list.stream())
+                .forEach(number -> System.out.println(number));
+    }
+}
---
+++ src/stream/FlatMapExample2.java
@@ -0,0 +1,15 @@
+package stream;
+
+import java.util.List;
+
+public class FlatMapExample2 {
+//    public static void main(String[] args) {
+//
+//        List<String> lists = List.of(
+//                "1asq_+","qewr90","bsmfhj56", "hfjjj7reyhs"
+//        );
+//
+//        lists.stream()
+//                .flatMap((phrase) -> phrase.g);
+//    }
+}
