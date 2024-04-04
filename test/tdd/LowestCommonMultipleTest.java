fbcf20fb0bdf244084c1c7fd07b932f9b2afd4b9
Adeshina Qudus <qudusa55@gmail.com>
2024-04-04 08:43:20 +0100

    hcf add one more test

--- test/tdd/LowestCommonMultipleTest.java
+++ test/tdd/LowestCommonMultipleTest.java
@@ -11,16 +11,22 @@ class LowestCommonMultipleTest {
 
 
     @Test
-    void divisorResultTest(){
+    void lcmFirstTest(){
         int [] numbers = {8,16,12};
         int result = 48;
         assertEquals(result,LowestCommonMultiple.lcm(numbers));
     }
 
     @Test
-    void lcmTest(){
+    void lcmSecondTest(){
         int [] numbers = {10,18,20};
         int result = 180;
+        assertEquals(result, LowestCommonMultiple.lcm(numbers));
+    }
+    @Test
+    void lcmThirdTest(){
+        int [] numbers = {12,18};
+        int result = 36;
         assertEquals(result, LowestCommonMultiple.lcm(numbers));
     }
 
