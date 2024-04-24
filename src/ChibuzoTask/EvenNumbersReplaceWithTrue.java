c4ac292c1b2a43ef29e274bc3aed8bf919abdceb
Adeshina Qudus <qudusa55@gmail.com>
2024-04-24 15:49:51 +0100

    ZerosAndOne

--- src/ChibuzoTask/EvenNumbersReplaceWithTrue.java
+++ src/ChibuzoTask/EvenNumbersReplaceWithTrue.java
@@ -6,7 +6,7 @@ public class EvenNumbersReplaceWithTrue {
     public static boolean[] evenNumberReplaceWithTrue(int[] input) {
         boolean [] zerosAndOne = new boolean[input.length];
         for (int count = 0 ; count < input.length;count++){
-            zerosAndOne[count] = input[count] % 2 == 0;
+            zerosAndOne[count] = input[count] % 2 != 0;
         }
         return zerosAndOne;
     }
